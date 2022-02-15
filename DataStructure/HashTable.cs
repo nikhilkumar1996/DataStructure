6ff483ebecffe61ff983db7d6d08ec24ded8d572
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-15 22:15:02 +0530

    [Nikhil Kumar]:Hash Table Program Added with UCs

---
+++ DataStructure/HashTable.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    //Value type Data type KeyValue
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    };
+    public class HashTable<K, V>
+    {
+        int size;
+        public LinkedList<KeyValue<K, V>>[] items;
+        public HashTable(int size)
+        {
+            this.size = size;
+            this.items=new LinkedList<KeyValue<K, V>>[size];
+        }
+        
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K,V>> LinkedListOfPosition = GetLinkedListOfPosition(position);
+            KeyValue<K, V> keyvalue = new KeyValue<K, V>()
+            {
+                Key = key,
+                Value = value
+            };
+            LinkedListOfPosition.AddLast(keyvalue);
+        }
+        //Step 1: Get array position
+        public int GetArrayPosition(K key)
+        {
+            int hashcode = key.GetHashCode();
+            int position = hashcode % size;
+            return Math.Abs(position);
+        }
+        //Step 2: Create linkedlist for a particular position
+        public LinkedList<KeyValue<K,V>> GetLinkedListOfPosition( int position)
+        {
+            if (items[position] == null)
+            {
+                items[position] = new LinkedList<KeyValue<K, V>>();
+            }
+            return items[position];
+        }
+        // Check if element is already Present
+        public int CheckHash(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListOfPosition = GetLinkedListOfPosition(position);
+            int count = 1;
+            bool found = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+
+            foreach (KeyValue<K, V> items in LinkedListOfPosition)
+            {
+                if (items.Key.Equals(key))
+                {
+                    count= Convert.ToInt32(items.Value) + 1;
+                    found = true;
+                    founditem = items;
+
+                }
+
+            }
+            if (found)
+            {
+                LinkedListOfPosition.Remove(founditem);
+                return count;
+
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        //Remove an iteam within a array
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListOfPosition = GetLinkedListOfPosition(position);
+            bool itemFound = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> keyValue in LinkedListOfPosition)
+            {
+                if (keyValue.Key.Equals(key))
+                {
+                    itemFound = true;
+                    founditem = keyValue;
+                }
+            }
+            if (itemFound)
+            {
+                LinkedListOfPosition.Remove(founditem);
+
+            }
+        }
+        //Display Linkedlist elements for particular key
+        public void Display(K key)
+        {
+             int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedListOfPosition = GetLinkedListOfPosition(position);
+            foreach (KeyValue<K, V> keyvalue in LinkedListOfPosition)
+            {
+                if (keyvalue.Key.Equals(key))
+                {
+                    Console.WriteLine("Key:"+ keyvalue.Key + "\t Value:" + keyvalue.Value);
+                }
+            }
+                
+        }
+            
+                
+            
+        
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,27 +11,144 @@ namespace DataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Program");
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.InsertionBetween(2, 30);
-            linkedList.Add(70);
-            linkedList.RemoveFirstNode();
-            linkedList.RemoveLastNode();
-            linkedList.Dislpay();
-            Stack stack = new Stack();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
-            stack.isEmpty();
-            Queue queue = new Queue();
-            queue.Enqueue(56);
-            queue.Enqueue(30);
-            queue.Enqueue(70);
-            queue.Dequeue();
-            queue.Display();
+            Console.WriteLine("1.\n LinkedList \n" +
+                              "2.\n Stack \n" +
+                              "3.\n Queue \n" +
+                              "4.\n HashTable for frequency of Words \n" +
+                              "5.\n HashTable for frequnecy of paragraph \n" +
+                              "6.\n HashTable for Removing a word \n" +
+                              "7.\n Exit \n ");
+            Console.WriteLine("Choose Program");
+            bool flag=true;
+            while (flag)
+            {
+                int choose=Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        LinkedList linkedList = new LinkedList();
+                        linkedList.Add(56);
+                        linkedList.InsertionBetween(2, 30);
+                        linkedList.Add(70);
+                        linkedList.RemoveFirstNode();
+                        linkedList.RemoveLastNode();
+                        linkedList.Dislpay();
+                        break;
+
+                    case 2:
+                        Stack stack = new Stack();
+                        stack.push(70);
+                        stack.push(30);
+                        stack.push(56);
+                        stack.Peek();
+                        stack.Pop();
+                        stack.Display();
+                        stack.isEmpty();
+                        break;
+
+                    case 3:
+                        Queue queue = new Queue();
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Dequeue();
+                        queue.Display();
+                        break;
+
+                    case 4:
+                        HashTable<string, int> hashtable = new HashTable<string, int>(6);
+                        string[] words = { "to", "be", "or", "not", "to", "be" };
+                        int count = 1;
+                        foreach (string word in words)
+                        {
+                            count = hashtable.CheckHash(word);
+                            if (count > 1)
+                            {
+                                hashtable.Add(word, count++);
+                            }
+                            else
+                            {
+                                hashtable.Add(word, 1);
+                            }
+                        }
+                        IEnumerable<string> uniqueItems = words.Distinct<string>();
+                        foreach (var i in uniqueItems)
+                        {
+                            hashtable.Display(i);
+                        }
+                        break;
+
+                    case 5:
+                        HashTable<string, int> hashtable1 = new HashTable<string, int>(10);
+                        string[] paragraph;
+
+                        string input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                        paragraph =input.Split(' '); //Given String Input
+                        
+                        int counts = 1;
+                        foreach (string word in paragraph)
+                        {
+                            count = hashtable1.CheckHash(word);
+                            if (counts > 1)
+                            {
+                                hashtable1.Add(word, counts++);
+                            }
+                            else
+                            {
+                                hashtable1.Add(word, 1);
+                            }
+                        }
+                        IEnumerable<string> distinctItems = paragraph.Distinct<string>();
+                        foreach (var i in distinctItems)
+                        {
+                            hashtable1.Display(i);
+                        }
+                        break;
+
+                    case 6:
+                        HashTable<string, int> myMap1 = new HashTable<string, int>(10);
+                        string[] paragraph1;
+                        string input1 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                        paragraph1 = input1.Split(' ');
+
+                        int count1 = 1;
+                        foreach (string i in paragraph1)
+                        {
+                            counts = myMap1.CheckHash(i);
+                            if (count1 > 1)
+                            {
+                                myMap1.Add(i, counts++);
+                            }
+                            else
+                            {
+                                myMap1.Add(i, 1);
+                            }
+                        }
+                        IEnumerable<string> unique = paragraph1.Distinct<string>();
+                        Console.WriteLine("\nEnter the word which you want to remove in paragraph");
+                        string removeWord = Console.ReadLine();
+                        myMap1.Remove(removeWord);
+                        foreach (var i in unique)
+                        {
+                            myMap1.Display(i);
+                        }
+                        break;
+
+
+                    case 7:
+                        flag = false;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                Console.ReadKey();
+            }
+            
+           
+            
 
 
 
